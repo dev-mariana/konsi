69bc2c8325335ffada440243a98c3ab95396b6d4
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-11-25 12:00:37 -0300

    lint: fix

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,18 @@ import { ZodError } from 'zod';
 import { appRoutes } from './application/controllers/routes';
 
 export const app = fastify({
-  logger: true,
+	logger: true,
 });
 
 app.register(appRoutes);
 
 app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply.status(400).send({
-      message: 'Validation error.',
-      issues: error.format(),
-    });
-  }
+	if (error instanceof ZodError) {
+		return reply.status(400).send({
+			message: 'Validation error.',
+			issues: error.format(),
+		});
+	}
 
-  return reply.status(500).send({ message: 'Internal server error.' });
+	return reply.status(500).send({ message: 'Internal server error.' });
 });
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app';
 import { env } from './infra/env';
 
 app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
-  console.log('Server is running...');
+	console.log('Server is running...');
 });
