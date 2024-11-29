c042e409b1d97e519799e1cd8a548706c5b09f2b
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-11-29 16:56:57 -0300

    refactor: remove unnecessary code

--- src/domain/services/save-benefits.service.ts
+++ src/domain/services/save-benefits.service.ts
@@ -23,15 +23,7 @@ export class SaveBenefitsService {
       token,
     );
 
-    const data = benefits?.map((benefit) => {
-      return {
-        tax_id: taxId,
-        number: benefit.number,
-        type_code: benefit.type_code,
-      };
-    });
-
-    await this.redisService.save(taxId, JSON.stringify(data));
+    await this.redisService.save(taxId, JSON.stringify(benefits));
 
     await this.sendQueueService.execute(taxId, benefits);
   }
