4c6f83e7953caaf7a37951db70265a4eb9e83401
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-11-29 16:56:29 -0300

    refactor: add null validation

--- src/infra/database/redis/redis.service.ts
+++ src/infra/database/redis/redis.service.ts
@@ -4,10 +4,17 @@ export class RedisService {
   constructor(private readonly client: FastifyRedis) {}
 
   async save(key: string, value: string): Promise<string> {
-    return await this.client.set(key, value);
+    const expiration = 30 * 60;
+    return await this.client.set(key, value, 'EX', expiration);
   }
 
   async get(key: string): Promise<string | null> {
-    return await this.client.get(key);
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    return JSON.parse(data);
   }
 }
